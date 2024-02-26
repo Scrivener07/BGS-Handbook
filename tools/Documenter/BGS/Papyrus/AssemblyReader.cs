081b92fbabe01cd25283c07d9be1fa7f4720498f
Scrivener07 <ScrivenerMods@gmail.com>
2024-02-26 06:52:26 -0500

    Update AssemblyReader.cs

--- tools/Documenter/BGS/Papyrus/AssemblyReader.cs
+++ tools/Documenter/BGS/Papyrus/AssemblyReader.cs
@@ -21,28 +21,36 @@ internal class AssemblyReader(string file) : StreamReader(file)
 
 	private static string Normalize(string line)
 	{
+		// Trim white space from beginning and ending.
+		// Replace back slashes with forward slashes.
 		return line.Trim().Replace("\\\\", "/");
 	}
 
 
 	private static string[] Tokenize(string line)
 	{
+		// First, cut the comment off the line ending.
 		line = line.Cut(';', out string? comment).Trim();
 		if (comment != null)
 			comment = comment.TrimStart(';').Trim();
 
+		// After cutting the comment, cut the quoted value.
 		line = line.Cut('"', out string? quoted).Trim();
 		if (quoted != null)
 			quoted = quoted.Trim('"').Trim();
 
+		// Split by space character for what remains of the line.
 		string[] tokens = line.Split(' ');
 
-		if (!string.IsNullOrWhiteSpace(comment))
-			tokens = tokens.Append(comment).ToArray();
-
+		// Add the quoted value.
 		if (!string.IsNullOrWhiteSpace(quoted))
 			tokens = tokens.Append(quoted).ToArray();
 
+		// Add comment token to end of array.
+		if (!string.IsNullOrWhiteSpace(comment))
+			tokens = tokens.Append(comment).ToArray();
+
+		// Return the tokenized line.
 		return tokens;
 	}
 
